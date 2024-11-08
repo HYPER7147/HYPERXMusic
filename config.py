4cb48e8b7933791aa701aa942c583768265dfac5
HYPER7147 <op0395687@gmail.com>
2024-11-08 22:19:52 +0530

    Hyper

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/HYPER7147",
+    "https://github.com/HYPER7147/HYPERXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
